7ae905751f77176640f8726815bad5e7ee04e894
thesaintzion <saintzion777@gmail.com>
2021-03-20 23:53:55 +0100

    added tailwind

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,13 @@ import Head from 'next/head'
 export default function Index() {
   return (
    <>
-   index
+   <p>pp</p>
+   {/* <header className="w-full text-white">
+
+   </header> */}
+{/* <div className="container mx-auto bg-black py-30 h-60">
+<button className="bg-green-500">Button</button>
+</div> */}
    </>
   )
 }
